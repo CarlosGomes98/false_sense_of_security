e5d39bf7e1aac490c10efbeecd7622ab0024574e
Carlos Gomes <carlosmiguel.gomes@live.com.pt>
2020-10-11 08:21:12 +0200

    Remove random step before pgd in gradient information

--- utils.py
+++ utils.py
@@ -28,14 +28,15 @@ def fgsm_(model, x, target, eps=0.5, targeted=True, device='cpu', clip_min=None,
         out.clamp_(min=clip_min, max=clip_max)
     return out
 
-def pgd_(model, x, target, step, eps, iters=7, targeted=True, device='cpu', clip_min=None, clip_max=None):
+def pgd_(model, x, target, step, eps, iters=7, targeted=True, device='cpu', clip_min=None, clip_max=None, random_step=True):
     projection_min = (x - eps).clamp_(min=clip_min)
     projection_max = (x + eps).clamp_(max=clip_max)
     
     # generate a random point in the +-eps box around x
-    offset = torch.rand_like(x)
-    offset = (offset*2*eps - eps)
-    x = x + offset
+    if random_step:
+      offset = torch.rand_like(x)
+      offset = (offset*2*eps - eps)
+      x = x + offset
     for _ in range(iters):
         x = fgsm_(model, x, target, eps=step, targeted=targeted, device=device, clip_min=None, clip_max=None)
         # project
@@ -47,7 +48,7 @@ def pgd_(model, x, target, step, eps, iters=7, targeted=True, device='cpu', clip
     return x
 
 def gradient_information(model, data, target, step=0.01, eps=0.5, iters=20, targeted=False, device='cpu', clip_min=None, clip_max=None):
-    adv = pgd_(model, data, target, step, eps, iters=iters, targeted=targeted, device=device, clip_min=clip_min, clip_max=clip_max).to(device)
+    adv = pgd_(model, data, target, step, eps, iters=iters, targeted=targeted, device=device, clip_min=clip_min, clip_max=clip_max, random_step=False).to(device)
     # only keep those for which an adversarial example was found
     new_labels = model(adv).argmax(axis=-1)
     adv_examples_index = new_labels != target
@@ -84,12 +85,12 @@ def gradient_norm(model, data, target, device='cpu'):
     grad_norm = torch.norm(grad, p=2, dim=1)
     return grad_norm.mean()
 
-def adversarial_accuracy(model, dataset_loader, attack=pgd_, iters=20, eps=0.5, step=0.1):
+def adversarial_accuracy(model, dataset_loader, attack=pgd_, iters=20, eps=0.5, step=0.1, random_step=True):
     correct = 0
     device = model.device
     for batch_idx, (data, target) in enumerate(dataset_loader):
         data, target = data.to(device), target.to(device)
-        adv = attack(model, data, target, step=step, eps=eps, iters=iters, targeted=False, device=device, clip_min=model.normalized_min, clip_max=model.normalized_max)
+        adv = attack(model, data, target, step=step, eps=eps, iters=iters, targeted=False, device=device, clip_min=model.normalized_min, clip_max=model.normalized_max, random_step=random_step)
         output = model(adv)
         pred = output.argmax(dim=1, keepdim=True)
         correct += pred.eq(target.view_as(pred)).sum().item()
