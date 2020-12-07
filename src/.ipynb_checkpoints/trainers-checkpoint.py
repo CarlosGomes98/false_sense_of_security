Add Linearizability test, fix gradient information using deepfool and logit difference as loss

src/.ipynb_checkpoints/trainers-checkpoint.py
@@ -9,7 +9,7 @@ from torch.autograd import grad
 import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.optim.lr_scheduler import StepLR
-from src.utils import fgsm_
+from src.utils import fgsm_, step_ll_
 from src.gradient_masking_tests import gradient_norm
 
 class Trainer:
@@ -100,13 +100,36 @@ class FGSMTrainer(Trainer):
                     epoch, batch_idx * len(data), len(train_loader.dataset),
                     100. * batch_idx / len(train_loader), loss.item()))
 
-# FROM https://github.com/F-Salehi/CURE_robustness
-# "Robustness via curvature regularization, and vice versa ", SM. Moosavi-Dezfooli, A. Fawzi, J. Uesato, and P. Frossard, CVPR 2019.
+class StepllTrainer(Trainer):
+    """
+    Extends base trainer class to implement training with a single FGSM step.
+    """
+    def __init__(self, device="cpu", log_interval=10, clip_min=0, clip_max=1, eps=(8/255), report_gradient_norm=None):
+        super(StepllTrainer, self).__init__(device=device, log_interval=log_interval, report_gradient_norm=report_gradient_norm)
+        self.clip_min = clip_min
+        self.clip_max = clip_max
+        self.eps = eps
+        
+    def train_step(self, model, train_loader, epoch, optimizer, criterion):
+        model.train()
+        for batch_idx, (data, target) in enumerate(train_loader):
+            data, target = data.to(self.device), target.to(self.device)
+            adv_data = step_ll_(model, data, target, eps=self.eps, device=self.device, clip_min=self.clip_min, clip_max=self.clip_max)
+            optimizer.zero_grad()
+            output = model(adv_data)
+            loss = criterion(output, target)
+            loss.backward()
+            optimizer.step()
+            if batch_idx % self.log_interval == 0:
+                print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                    epoch, batch_idx * len(data), len(train_loader.dataset),
+                    100. * batch_idx / len(train_loader), loss.item()))
+
+
 class GradientRegularizationTrainer(Trainer):
     """
     Extends the base trainer class to implement training with input gradient regularization.
 
-    TODO: Decide on good value for lambda, good annealing schedule.
     """
     def __init__(self, device="cpu", log_interval=10, lambda_=0.1, annealing=False, report_gradient_norm=None):
         super(GradientRegularizationTrainer, self).__init__(device=device, log_interval=log_interval, report_gradient_norm=report_gradient_norm)
@@ -137,7 +160,13 @@ class GradientRegularizationTrainer(Trainer):
             output = model(data)
             loss = criterion(output, target)
             ce_loss = loss
-            gradient_norm = (grad(outputs=loss, inputs=data, retain_graph=True, only_inputs=True)[0]**2).sum()
+            for i in range(output.shape[1]):
+                model.zero_grad()
+                if i == 0:
+                    norm = grad(outputs=output[0, i], inputs=data, retain_graph=True, only_inputs=True)[0]
+                else:
+                    norm = torch.cat([norm, grad(outputs=output[0, i], inputs=data, retain_graph=True, only_inputs=True)[0]])
+            gradient_norm = torch.norm(norm)
             loss = loss + self.cur_lambda * gradient_norm
             optimizer.zero_grad()
             loss.backward()

src/.ipynb_checkpoints/utils-checkpoint.py
@@ -4,6 +4,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+from foolbox import PyTorchModel, accuracy, samples
+from foolbox.attacks import LinfPGD, FGSM, LinfDeepFoolAttack
 
 def fgsm_(model, x, target, eps=0.5, targeted=True, device='cpu', clip_min=None, clip_max=None, **kwargs):
     """
@@ -109,7 +111,7 @@ def pgd_(model, x, target, step, eps, iters=7, targeted=True, device='cpu', clip
                 break
     return x
 
-def gradient_information(model, data, target, step=0.01, eps=0.5, iters=20, targeted=False, device='cpu', clip_min=None, clip_max=None):
+def gradient_information(model, data, target, iters=50, device='cpu'):
     """
     Computes the cosine information between the gradient of point at the decision boundary w.r.t. the loss and the vector (point at decision boundary - original input point).
 
@@ -119,8 +121,11 @@ def gradient_information(model, data, target, step=0.01, eps=0.5, iters=20, targ
     TODO: Incorrect implementation!!!!!! Do not use PGD, use something like deepfool. And do not use loss function for decision boundary, but rather difference in logits.
     TODO: Move to metrics
     """
-    
-    adv = pgd_(model, data, target, step, eps, iters=iters, targeted=targeted, device=device, clip_min=clip_min, clip_max=clip_max, random_step=False, early_stop=True).to(device)
+    fmodel = PyTorchModel(model, bounds=(0, 1))
+    attack = LinfDeepFoolAttack(overshoot=0.002, steps=iters)
+    data = data.to(device)
+    target = target.to(device)
+    _, adv, success = attack(fmodel, data, target, epsilons=10)
     # only keep those for which an adversarial example was found
     new_labels = model(adv).argmax(axis=-1)
     adv_examples_index = new_labels != target
@@ -136,12 +141,12 @@ def gradient_information(model, data, target, step=0.01, eps=0.5, iters=20, targ
 
     model.zero_grad()
     logits = model(adv)
-    loss = nn.CrossEntropyLoss()(logits, target[adv_examples_index])
+    loss = torch.sum((logits[target] - logits[new_labels])**2)
     loss.backward()
 
     grad = adv.grad.reshape(adv.shape[0], -1)
     diff_vector = (adv - data[adv_examples_index]).reshape(adv.shape[0], -1)
-    cos = nn.CosineSimilarity(dim=1, eps=1e-12)
+    cos = nn.CosineSimilarity(dim=1, eps=1e-18)
     grad_information[adv_examples_index] = cos(grad, diff_vector)
     return grad_information
 

src/gradient_masking_tests.py
@@ -58,11 +58,12 @@ def run_masking_benchmarks(model, test_dataset, epsilon=0.06, device="cpu", batc
     print("SPSA accuracy - eps = {}: {}%".format(epsilon, spsa_acc))
     print("SPSA accuracy - eps = {}: {}%".format(epsilon/2, spsa_acc_small))
     
-    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8,10))
-    ax1.plot(epsilons, fgsm_acc)
-    ax1.set_title("FGSM Accuracy")
-    ax2.plot(epsilons, random_acc)
-    ax2.set_title("Random Attack Accuracy") 
+    fig = plt.figure(figsize=(12,8))
+    plt.plot(epsilons, fgsm_acc, label='FGSM Accuracy')
+    plt.plot(epsilons, random_acc, label= 'Random Attack Accuracy')
+    plt.xlabel('Epsilon')
+    plt.ylabel('Accuracy (%)')
+    plt.legend()
     pbar.update(1)
     pbar.close()
 
@@ -143,8 +144,47 @@ def gradient_norm(model, data_loader, device='cpu', subset_size=10000):
     grad_norm = torch.cat(grad_norms)
     return grad_norm
 
+def gradient_information(model, dataset, iters=50, device='cpu', subset_size=1000):
+    """
+    Computes the cosine information between the gradient of point at the decision boundary w.r.t. the different in logits and the vector (point at decision boundary - original input point).
 
-def fgsm_pgd_cos_dif(model, test_dataset, epsilon=0.03, subset_size=1000, device="cpu", batch_size=32, n_steps_pgd=7, return_adjusted_fgsm=True):
+    For non gradient masked models, this point should be the closest one to the input that is at the decision boundary.
+    Thus, we would expect these vectors to be +- collinear.
+    """
+    fmodel = PyTorchModel(model, bounds=(0, 1))
+    attack = LinfDeepFoolAttack(overshoot=0.002, steps=iters)
+    subset = torch.utils.data.Subset(dataset, np.random.randint(0, len(dataset), size=subset_size).tolist())
+    subset_loader = torch.utils.data.DataLoader(subset, batch_size=128,
+                            shuffle=False, num_workers=2)
+    grad_information_full = []
+    for data, target in subset_loader:
+        data = data.to(device)
+        target = target.to(device)
+        _, adv, success = attack(fmodel, data, target, epsilons=8)
+        # only keep those for which an adversarial example was found
+        new_labels = model(adv).argmax(axis=-1)
+        adv_examples_index = new_labels != target
+        # print("{} adv. examples found from {} data points".format(adv_examples_index.sum().item(), data.shape[0]))
+        if adv_examples_index.sum() == 0:
+            return None
+        
+        grad_information = torch.Tensor(adv.shape[0]).to(device)
+        grad_information[~adv_examples_index] = float('nan')
+        
+        adv = adv[adv_examples_index].detach().clone()
+        adv.requires_grad = True
+        model.zero_grad()
+        logits = model(adv)
+        loss = torch.sum(logits[:, new_labels] - logits[:, target])
+        loss.backward()
+        grad = adv.grad.reshape(adv.shape[0], -1)
+        diff_vector = (adv - data[adv_examples_index]).reshape(adv.shape[0], -1)
+        cos = nn.CosineSimilarity(dim=1, eps=1e-18)
+        grad_information[adv_examples_index] = cos(grad, diff_vector)
+        grad_information_full.append(grad_information)
+    return torch.cat(grad_information_full).mean()
+
+def fgsm_pgd_cos_dif(model, test_dataset, epsilons=[0.03], subset_size=1000, device="cpu", batch_size=32, n_steps_pgd=7, return_adjusted_fgsm=True):
     '''
     Method that evaluates how informative the gradients of the network are. Preforms pgd and fgsm and compares the solutions.
     Returns the cosine difference and euclidian distance between the solutions.
@@ -153,38 +193,47 @@ def fgsm_pgd_cos_dif(model, test_dataset, epsilon=0.03, subset_size=1000, device
     was often very close to 1, yet the norm was quite different.
     '''
     fmodel = PyTorchModel(model, bounds=(0, 1))
-    cos_dif = []
-    distance = []
-    successes_fgsm = []
-    successes_pgd = []
-    if return_adjusted_fgsm:
-        successes_adjusted_fgsm = []
-    subset = torch.utils.data.Subset(test_dataset, np.random.randint(0, len(test_dataset), size=subset_size).tolist())
-    subset_loader = torch.utils.data.DataLoader(subset, batch_size=batch_size,
-                                 shuffle=False, num_workers=2)
-    for images, labels in tqdm(subset_loader):
-        images, labels = images.to(device), labels.type(torch.cuda.LongTensor)
-        _, advs_fgsm, success_fgsm = FGSM()(fmodel, images, labels, epsilons=epsilon)
-        _, advs_pgd, success_pgd = LinfPGD(steps=n_steps_pgd, rel_stepsize=1/4)(fmodel, images, labels, epsilons=epsilon)
-        fgsm_perturbation = advs_fgsm - images
-        pgd_perturbation = advs_pgd - images
+    for epsilon in epsilons:
+        cos_dif = []
+        distance = []
+        successes_fgsm = []
+        successes_pgd = []
         if return_adjusted_fgsm:
-            adjusted_fgsm = ((fgsm_perturbation / torch.linalg.norm(fgsm_perturbation.reshape(advs_fgsm.shape[0], -1), dim=1).reshape(advs_fgsm.shape[0], 1, 1, 1))
-            * torch.linalg.norm(pgd_perturbation.reshape(advs_pgd.shape[0], -1), dim=1).reshape(advs_fgsm.shape[0], 1, 1, 1)) + images
-            _, _, success_adjusted_fgsm = FGSM()(fmodel, adjusted_fgsm, labels, epsilons=0) # this is a hack to get the successes. Can be done more efficiently
-            successes_adjusted_fgsm.append(success_adjusted_fgsm)
-        fgsm_perturbation = fgsm_perturbation.reshape(fgsm_perturbation.shape[0], -1)
-        pgd_perturbation = pgd_perturbation.reshape(pgd_perturbation.shape[0], -1)
-        cos = nn.CosineSimilarity(dim=1, eps=1e-18)
-        cos_dif.append(cos(fgsm_perturbation, pgd_perturbation))
-        dist = torch.linalg.norm(fgsm_perturbation - pgd_perturbation, dim=1, ord=2)
-        distance.append(dist)
-        successes_fgsm.append(success_fgsm)
-        successes_pgd.append(success_pgd)
+            successes_adjusted_fgsm = []
+        subset = torch.utils.data.Subset(test_dataset, np.random.randint(0, len(test_dataset), size=subset_size).tolist())
+        subset_loader = torch.utils.data.DataLoader(subset, batch_size=batch_size,
+                                    shuffle=False, num_workers=2)
+        for images, labels in tqdm(subset_loader):
+            images, labels = images.to(device), labels.type(torch.cuda.LongTensor)
+            _, advs_fgsm, success_fgsm = FGSM()(fmodel, images, labels, epsilons=epsilon)
+            _, advs_pgd, success_pgd = LinfPGD(steps=n_steps_pgd, rel_stepsize=1/4)(fmodel, images, labels, epsilons=epsilon)
+            fgsm_perturbation = advs_fgsm - images
+            pgd_perturbation = advs_pgd - images
+            if return_adjusted_fgsm:
+                adjusted_fgsm = ((fgsm_perturbation / torch.linalg.norm(fgsm_perturbation.reshape(advs_fgsm.shape[0], -1), dim=1).reshape(advs_fgsm.shape[0], 1, 1, 1))
+                * torch.linalg.norm(pgd_perturbation.reshape(advs_pgd.shape[0], -1), dim=1).reshape(advs_fgsm.shape[0], 1, 1, 1)) + images
+                _, _, success_adjusted_fgsm = FGSM()(fmodel, adjusted_fgsm, labels, epsilons=0) # this is a hack to get the successes. Can be done more efficiently
+                successes_adjusted_fgsm.append(success_adjusted_fgsm)
+            fgsm_perturbation = fgsm_perturbation.reshape(fgsm_perturbation.shape[0], -1)
+            pgd_perturbation = pgd_perturbation.reshape(pgd_perturbation.shape[0], -1)
+            cos = nn.CosineSimilarity(dim=1, eps=1e-18)
+            cos_dif.append(cos(fgsm_perturbation, pgd_perturbation))
+            dist = torch.linalg.norm(fgsm_perturbation - pgd_perturbation, dim=1, ord=2)
+            distance.append(dist)
+            successes_fgsm.append(success_fgsm)
+            successes_pgd.append(success_pgd)
+        
+        print("Epsilon = {}:".format(epsilon))
+        cos_dif, distance, successes_fgsm, successes_pgd = torch.cat(cos_dif), torch.cat(distance), torch.cat(successes_fgsm), torch.cat(successes_pgd)
+        print("Mean Cosine Difference: {}, Mean Cosine Difference when FGSM does not succeed but PGD does: {}, Mean l2 Distance: {}".format(cos_dif[success_fgsm].mean(), cos_dif[(~success_fgsm & success_pgd)].mean(), dist.mean()))
+        if return_adjusted_fgsm:
+            success_adjusted_fgsm = torch.cat(successes_adjusted_fgsm)
+            print("FGSM success: {}, PGD Success: {}, Rescaled FGSM success: {}".format(success_fgsm.sum()/subset_size, success_pgd.sum()/subset_size, success_adjusted_fgsm.sum()/subset_size))
+        else:
+            print("FGSM success: {}, PGD Success: {}, Rescaled FGSM success: {}".format(success_fgsm.sum()/subset_size, success_pgd.sum()/subset_size, success_adjusted_fgsm.sum()/subset_size))
+        
      
-    if return_adjusted_fgsm:
-        return torch.cat(cos_dif), torch.cat(distance), torch.cat(successes_fgsm), torch.cat(successes_pgd), torch.cat(successes_adjusted_fgsm)
-    return torch.cat(cos_dif), torch.cat(distance), torch.cat(successes_fgsm), torch.cat(successes_pgd)
+    
 
 '''
 Method that preforms an fgsm attack at a range of epsilons

src/train_model.py
@@ -20,7 +20,7 @@ NAME_TO_MODEL = {'wide_res_net': CIFAR_Wide_Res_Net, 'res_net': CIFAR_Res_Net, '
 Train a given model architecture with a given robustness strategy
 Can also save the norm of gradients w.r.t. input at each iteration
 '''
-def train_model(model_name, strategy, output_path, epochs, eps=None, report_gradient_norm=False, lambda_=None):
+def train_model(model_name, strategy, output_path, epochs, eps=None, report_gradient_norm=False, lambda_=None, model_path=None):
     # setup
     device = torch.device("cuda")
     batch_size = 128
@@ -46,6 +46,8 @@ def train_model(model_name, strategy, output_path, epochs, eps=None, report_grad
         report_gradient_norm = None
 
     model = NAME_TO_MODEL[model_name](device)
+    if model_path is not None:
+        model.load_state_dict(torch.load(model_path))
     optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
 
     if strategy == 'normal':
@@ -81,10 +83,11 @@ if __name__ == "__main__":
     parser.add_argument('--epochs', type=int, default=10, help='epochs to train for')
     parser.add_argument('--output_path', type=str, default='models\output.model', help='name of the model')
     parser.add_argument('--report_gradient_norm', action='store_true', help='Will save gradient norms to directory at output path')
+    parser.add_argument('--model_path', type=str, default=None, help='Allows a model to be provided, which will be trained on')
 
 
     
 
     args = parser.parse_args()
 
-    train_model(args.model, args.strategy, args.output_path, args.epochs, eps=args.eps, report_gradient_norm=args.report_gradient_norm, lambda_=args.lambda_)
+    train_model(args.model, args.strategy, args.output_path, args.epochs, eps=args.eps, report_gradient_norm=args.report_gradient_norm, lambda_=args.lambda_, model_path=args.model_path)

src/trainers.py
@@ -12,6 +12,7 @@ from torch.optim.lr_scheduler import StepLR
 from src.utils import fgsm_, step_ll_
 from src.gradient_masking_tests import gradient_norm
 
+lr = 0.001
 class Trainer:
     """
     Base class that trains a given model, given a dataloader using standard SGD.
@@ -30,7 +31,7 @@ class Trainer:
         
     def train(self, model, train_loader, epochs, test_loader=None, optimizer=None):
         if optimizer is None:
-            optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
+            optimizer = optim.SGD(model.parameters(), lr=lr, momentum=0.9)
         criterion = nn.CrossEntropyLoss()
         for epoch in range(1, epochs + 1):
             self.train_step(model, train_loader, epoch, optimizer, criterion)
@@ -130,7 +131,6 @@ class GradientRegularizationTrainer(Trainer):
     """
     Extends the base trainer class to implement training with input gradient regularization.
 
-    TODO: Decide on good value for lambda, good annealing schedule?
     """
     def __init__(self, device="cpu", log_interval=10, lambda_=0.1, annealing=False, report_gradient_norm=None):
         super(GradientRegularizationTrainer, self).__init__(device=device, log_interval=log_interval, report_gradient_norm=report_gradient_norm)
@@ -140,7 +140,7 @@ class GradientRegularizationTrainer(Trainer):
 
     def train(self, model, train_loader, epochs, test_loader=None, optimizer=None):
         if optimizer is None:
-            optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
+            optimizer = optim.SGD(model.parameters(), lr=lr, momentum=0.9)
         criterion = nn.CrossEntropyLoss()
         for epoch in range(1, epochs + 1):
             self.train_step(model, train_loader, epoch, optimizer, criterion)
@@ -161,7 +161,13 @@ class GradientRegularizationTrainer(Trainer):
             output = model(data)
             loss = criterion(output, target)
             ce_loss = loss
-            gradient_norm = (grad(outputs=loss, inputs=data, retain_graph=True, only_inputs=True)[0]**2).sum()
+            for i in range(output.shape[1]):
+                model.zero_grad()
+                if i == 0:
+                    norm = grad(outputs=output[0, i], inputs=data, retain_graph=True, only_inputs=True)[0]
+                else:
+                    norm = torch.cat([norm, grad(outputs=output[0, i], inputs=data, retain_graph=True, only_inputs=True)[0]])
+            gradient_norm = torch.norm(norm)
             loss = loss + self.cur_lambda * gradient_norm
             optimizer.zero_grad()
             loss.backward()
@@ -223,7 +229,7 @@ class CurvatureRegularizationTrainer(Trainer):
     
     def train(self, model, train_loader, epochs, test_loader=None, optimizer=None, h=[0.1, 0.4, 0.8, 1.8, 3]):
         if optimizer is None:
-            optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
+            optimizer = optim.SGD(model.parameters(), lr=lr, momentum=0.9)
         criterion = nn.CrossEntropyLoss()
         for epoch in range(1, epochs + 1):
             self.train_step(model, train_loader, epoch, optimizer, criterion, h=h)

src/utils.py
@@ -1,9 +1,12 @@
 # some of the code from RAI
 # This file contains utility functions used throughout the project.
 import torch
+import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+from foolbox import PyTorchModel, accuracy, samples
+from foolbox.attacks import LinfPGD, FGSM, LinfDeepFoolAttack
 
 def fgsm_(model, x, target, eps=0.5, targeted=True, device='cpu', clip_min=None, clip_max=None, **kwargs):
     """
@@ -108,42 +111,6 @@ def pgd_(model, x, target, step, eps, iters=7, targeted=True, device='cpu', clip
             if done.all():
                 break
     return x
-
-def gradient_information(model, data, target, step=0.01, eps=0.5, iters=20, targeted=False, device='cpu', clip_min=None, clip_max=None):
-    """
-    Computes the cosine information between the gradient of point at the decision boundary w.r.t. the loss and the vector (point at decision boundary - original input point).
-
-    For non gradient masked models, this point should be the closest one to the input that is at the decision boundary.
-    Thus, we would expect these vectors to be +- collinear.
-
-    TODO: Incorrect implementation!!!!!! Do not use PGD, use something like deepfool. And do not use loss function for decision boundary, but rather difference in logits.
-    TODO: Move to metrics
-    """
-    
-    adv = pgd_(model, data, target, step, eps, iters=iters, targeted=targeted, device=device, clip_min=clip_min, clip_max=clip_max, random_step=False, early_stop=True).to(device)
-    # only keep those for which an adversarial example was found
-    new_labels = model(adv).argmax(axis=-1)
-    adv_examples_index = new_labels != target
-    print("{} adv. examples found from {} data points".format(adv_examples_index.sum().item(), data.shape[0]))
-    if adv_examples_index.sum() == 0:
-        return None
-    
-    grad_information = torch.Tensor(adv.shape[0]).to(device)
-    grad_information[~adv_examples_index] = float('nan')
-    
-    adv = adv[adv_examples_index].detach().clone()
-    adv.requires_grad = True
-
-    model.zero_grad()
-    logits = model(adv)
-    loss = nn.CrossEntropyLoss()(logits, target[adv_examples_index])
-    loss.backward()
-
-    grad = adv.grad.reshape(adv.shape[0], -1)
-    diff_vector = (adv - data[adv_examples_index]).reshape(adv.shape[0], -1)
-    cos = nn.CosineSimilarity(dim=1, eps=1e-12)
-    grad_information[adv_examples_index] = cos(grad, diff_vector)
-    return grad_information
 
 def adversarial_accuracy(model, dataset_loader, attack=pgd_, iters=20, eps=0.5, step=0.1, random_step=False, device="cpu"):
     """